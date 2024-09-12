d5895bdf7df2750909874948ade58cb52a984e31
nguyenduchoaang <duchoaang.dev@gmail.com>
2024-09-13 00:28:26 +0700

    update teacher ui

--- src/entities/homepage/teacher/index.tsx
+++ src/entities/homepage/teacher/index.tsx
@@ -1,14 +1,15 @@
-import { BaseWrapperWithColor } from '../BaseWrapper';
+import { BaseWrapperBasic } from '../BaseWrapper';
 import Teacher1 from '@/assets/teacher/Le-Duy-Anh.jpg';
+import { CertificateIcon } from '@/based/config/SVGIcon';
 import { useState } from 'react';
-
+import { motion, AnimatePresence } from 'framer-motion';
 const ListTeacher = [
   {
     id: 1,
     img: Teacher1,
     name: 'Nguyễn Thị A',
     position: 'Giáo viên',
-    listCertificate: ['Chứng chỉ giáo viên', 'Chứng chỉ giáo viên', 'Chứng chỉ giáo viên', 'Chứng chỉ giáo viên'],
+    listCertificate: ['Chứng chỉ giáo viên', 'Chứng chỉ Mosteri', 'Chứng chỉ giáo viên', 'Chứng chỉ giáo viên'],
   },
   {
     id: 2,
@@ -37,7 +38,7 @@ const Teacher = () => {
   const [selectedTeacher, setSelectedTeacher] = useState(ListTeacher[0]);
 
   return (
-    <BaseWrapperWithColor className="flex flex-col p-8">
+    <BaseWrapperBasic className="flex flex-col p-8">
       <div className="w-[60%] mx-auto">
         <h2 className="text-2xl font-bold mb-4">Đội ngũ giáo viên</h2>
         <p className="mb-8">
@@ -48,7 +49,7 @@ const Teacher = () => {
             <div
               key={index}
               className={`text-center cursor-pointer border-[1px] flex justify-center items-center flex-col w-[23%]  py-3 ${
-                selectedTeacher.id === teacher.id ? 'border-[#0065FF]' : 'border-[#c7cdda]'
+                selectedTeacher.id === teacher.id ? 'border-[#0065FF] bg-[#dcedfd]' : 'border-[#c7cdda]'
               } rounded-lg`}
               onClick={() => setSelectedTeacher(teacher)}
             >
@@ -57,22 +58,41 @@ const Teacher = () => {
             </div>
           ))}
         </div>
-        <div className="teacher-info grid grid-cols-[40%,65%] ">
-          <div className="border-[1px] border-[#c7cdda] rounded-2xl max-h-[240px] bg-white flex justify-center">
-            <img src={selectedTeacher.img} alt={selectedTeacher.name} className="w-[50%] h-full  mb-4" />
+        <AnimatePresence key={selectedTeacher.id}>
+          <div className="teacher-info grid grid-cols-[40%,65%] ">
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+              className="border-[1px] border-[#c7cdda] rounded-2xl max-h-[240px] bg-white flex justify-center"
+            >
+              <img src={selectedTeacher.img} alt={selectedTeacher.name} className="w-[50%] h-full  mb-4" />
+            </motion.div>
+            <motion.div initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} className="ml-3">
+              <h3 className="font-semibold">{selectedTeacher.name}</h3>
+              <p className="mb-3 text-[13px]">{selectedTeacher.position}</p>
+              <ul className="grid grid-cols-2">
+                {selectedTeacher.listCertificate.map((certificate, index) => (
+                  <li key={index} className="flex">
+                    <span className="mr-2 p-2 bg-[#dcedfd] mb-5 rounded-md">
+                      {index == 0 ? (
+                        <CertificateIcon.Certificate1 width="22px" height="22px" />
+                      ) : (
+                        <CertificateIcon.Certificate2 width="22px" height="22px" />
+                      )}
+                    </span>
+                    <div className="flex flex-col ">
+                      <p className="text-[14px] font-semibold">{certificate}</p>
+                      <span className="text-[12px] font-normal">5.6</span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
           </div>
-          <div className="ml-3">
-            <h3 className="font-semibold mb-1 x">{selectedTeacher.name}</h3>
-            <p className="mb-1 text-[13px]">{selectedTeacher.position}</p>
-            <ul className="">
-              {selectedTeacher.listCertificate.map((certificate, index) => (
-                <li key={index}>{certificate}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        </AnimatePresence>
       </div>
-    </BaseWrapperWithColor>
+    </BaseWrapperBasic>
   );
 };
 
